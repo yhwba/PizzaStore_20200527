c3bb7bf0d3ebe8d595d6b6931c7f052010d8e24e
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-27 10:56:49 +0900

    피자 가게들을 ArrayList로 추가  - 이미지 주소가 String으로 첨부

--- app/src/main/java/kr/co/yhw/pizzastore_20200527/MainActivity.java
+++ app/src/main/java/kr/co/yhw/pizzastore_20200527/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import kr.co.yhw.pizzastore_20200527.databinding.ActivityMainBinding;
+import kr.co.yhw.pizzastore_20200527.datas.PizzaStore;
 
 public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding;
+
+    List<PizzaStore> pizzaStoreList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +33,17 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setValues() {
 
+        addpizzaStoreList();
+
     }
+
+    void addpizzaStoreList(){
+        pizzaStoreList.add(new PizzaStore("피자헛","1588-0088","https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"));
+        pizzaStoreList.add(new PizzaStore("파파존스","1521-0088","http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800"));
+        pizzaStoreList.add(new PizzaStore("미스터피자","1566-0008","https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200"));
+        pizzaStoreList.add(new PizzaStore("도미노피자","1778-0088","https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png"));
+    }
+
+
+
 }
