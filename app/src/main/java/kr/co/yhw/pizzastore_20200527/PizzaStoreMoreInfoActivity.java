bc1cde4284ad98a15eb0aab02868c7e790a9f71d
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-27 12:55:42 +0900

    피자가게 상세정보 실제 출력

--- app/src/main/java/kr/co/yhw/pizzastore_20200527/PizzaStoreMoreInfoActivity.java
+++ app/src/main/java/kr/co/yhw/pizzastore_20200527/PizzaStoreMoreInfoActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 
 import androidx.databinding.DataBindingUtil;
 
+import com.bumptech.glide.Glide;
+
 import kr.co.yhw.pizzastore_20200527.databinding.ActivityPizzaStoreMoreInfoBinding;
+import kr.co.yhw.pizzastore_20200527.datas.PizzaStore;
 
 public class PizzaStoreMoreInfoActivity extends BaseActivity {
 
     ActivityPizzaStoreMoreInfoBinding binding;
+
+    PizzaStore mStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,11 @@ public class PizzaStoreMoreInfoActivity extends BaseActivity {
     @Override
     public void setValues() {
 
+        mStore = (PizzaStore) getIntent().getSerializableExtra("store");
+        binding.nameTxt.setText(mStore.getName());
+        binding.phoneNumTxt.setText(mStore.getPhoneNum());
 
+        Glide.with(mContext).load(mStore.getLogoImgUrl()).into(binding.logoImg);
 
     }
 }
