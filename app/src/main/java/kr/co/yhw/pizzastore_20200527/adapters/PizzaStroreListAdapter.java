76cd6c5835435b7543b15393104e1dd513e849b9
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-27 10:51:03 +0900

    피자가게 뿌려줄 어댑터 뼈대 작업

--- app/src/main/java/kr/co/yhw/pizzastore_20200527/adapters/PizzaStroreListAdapter.java
+++ app/src/main/java/kr/co/yhw/pizzastore_20200527/adapters/PizzaStroreListAdapter.java
@@ -5,13 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.List;
 
+import kr.co.yhw.pizzastore_20200527.R;
 import kr.co.yhw.pizzastore_20200527.datas.PizzaStore;
 
 public class PizzaStroreListAdapter extends ArrayAdapter<PizzaStore> {
@@ -20,34 +20,24 @@ public class PizzaStroreListAdapter extends ArrayAdapter<PizzaStore> {
     List<PizzaStore> mList;
     LayoutInflater inf;
 
-    public PizzaStroreListAdapter (@NonNull Context context, int resource, @NonNull List<PizzaStore> objects) {
+    public PizzaStroreListAdapter(@NonNull Context context, int resource, @NonNull List<PizzaStore> objects) {
         super(context, resource, objects);
         mContext = context;
         mList = objects;
         inf = LayoutInflater.from(mContext);
 
     }
+
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View row = convertView;
-
-        if (row == null){
-            row = inf.inflate(R.layout.room_list_item, null);
+        if (row == null) {
+            row = inf.inflate(R.layout.pizza_store_list_item, null);
         }
-        TextView priceTxt = row.findViewById(R.id.priceTxt);
-        TextView addressAndFloorTxt = row.findViewById(R.id.addressAndFloorTxt);
-        TextView descTxt = row.findViewById(R.id.descTxt);
 
-        PizzaStore data = mList.get(position);
-
-
-        storeNameTxt.setText(data.getFormattedPrice());
-        addressAndFloorTxt.setText(String.format("%s,%s",data.getAddress(),data.getFormattedFloor()));
-        descTxt.setText(data.getDescription());
 
         return row;
     }
-
-
 }
